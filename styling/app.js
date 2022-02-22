13cf119a3497f34e4d87382691f09e5280850875
Joanna <96662133+Joanna32@users.noreply.github.com>
2022-02-22 20:46:44 +0100

    change css and js

--- styling/app.js
+++ styling/app.js
@@ -1,9 +1,11 @@
 function showBirthdayWishes() {
   let heading = document.querySelector("#birthday-heading");
-  heading.setAttribute("src", "image/wishes.jpg");
+  heading.innerHTML = "HAPPY BIRTHDAY!!";
   let content = document.querySelector("#birthday-wishes");
   content.innerHTML =
-    "Wishing You all the best! Have a great day and a lot of fun today :) P.S You are older today than yesterday but younger than tomorrow😎";
+    "Wishing You all the best! Have a great day and a lot of fun today:) You are older today than yesterday but younger than tomorrow 😎";
+  let footer = document.querySelector("#name");
+  footer.innerHTML = "Joanna";
 }
 
 let button = document.querySelector("#click-me");
